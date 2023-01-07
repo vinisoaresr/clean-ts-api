e18006dc1c46423d453ff1641ad13d9e75bfad3a
vinisoaresr <vinisoaresr@hotmail.com>
2023-01-07 10:48:39 -0300

    feat: ensure dbAddAccount has been called with correct value

--- src/data/useCases/add-account/db-add-account-protocols.ts
+++ src/data/useCases/add-account/db-add-account-protocols.ts
@@ -1,3 +1,4 @@
 export * from '../../../domain/useCases/add-account/addAccount'
 export * from '../../../domain/models/account'
 export * from '../../protocols/encrypter'
+export * from '../../protocols/addAccountRepository'
