f97a3b48c1dac867ff4ab51d36a5ca4f8967f251
vinisoaresr <vinisoaresr@hotmail.com>
2023-01-07 21:42:12 -0300

    feat: ensure MongoDbHelper reconnect if connection is down

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,17 +2,23 @@ import { Collection, MongoClient } from 'mongodb'
 
 class MongoHelpers {
   private client: MongoClient
+  private URL: string
 
   async connect (URL: string): Promise<void> {
     this.client = await MongoClient.connect(URL)
+    this.URL = URL
   }
 
   async disconnect (): Promise<void> {
     await this.client.close()
+    this.client = null
   }
 
-  getCollection (name: string): Collection {
-    return this.client.db().collection(name)
+  async getCollection (name: string): Promise<Collection> {
+    if (!this.client) {
+      await this.connect(this.URL)
+    }
+    return new Promise(resolve => resolve(this.client.db().collection(name)))
   }
 
   mapToEntity (account: any): any {
