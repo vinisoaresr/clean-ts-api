4f40a5b4b70a80e7e8feec0330ed1e36d8342521
vinisoaresr <vinisoaresr@hotmail.com>
2023-01-08 09:36:40 -0300

    feat: ensure LogControllerDecorator calls internal handle method with correct param

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,12 +3,15 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/useCases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { Controller } from '../../presentation/protocols'
+import { LogControllerDecorator } from '../decorator/log'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12
   const bcrypt = new BcryptAdapter(salt)
   const emailValidator = new EmailValidatorAdapter()
   const addAccountRepo = new AccountMongoRepository()
   const addAccount = new DbAddAccount(bcrypt, addAccountRepo)
-  return new SignUpController(emailValidator, addAccount)
+  const signUpController = new SignUpController(emailValidator, addAccount)
+  return new LogControllerDecorator(signUpController)
 }
